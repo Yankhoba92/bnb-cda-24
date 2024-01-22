253a1f17343ecd245681c017005dfa66ac6d704f
Yankhoba <bagayoko.yan@gmail.com>
2024-01-22 11:21:48 +0100

    📝 Mise en place de la pagination

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\RoomRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,11 +15,21 @@ class RoomController extends AbstractController
     #[Route('/', name: 'app_room')]
     public function index(
         RoomRepository $roomRepository,
+        PaginatorInterface $paginator,
+        Request $request
     ): Response
 
     {
+        $pagination = $paginator->paginate(
+            $roomRepository->findAll(), // All rooms
+            $request->query->getInt('page', 1), // Check page number
+            12 // items par page
+        );
         return $this->render('room/index.html.twig', [
-            'rooms' => $roomRepository->findAll(),
+            'rooms' => $pagination,
+            'hostRooms' =>$roomRepository->findBy(
+                ['host' => $this->getUser()]
+            )
         ]);
     }
 }
