2368a7d09d40c3e754ee77415ad5ed4628b8ff1b
Yankhoba <bagayoko.yan@gmail.com>
2024-01-26 09:24:25 +0100

    Dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,8 +7,9 @@ use App\Entity\User;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\Security\Core\User\UserInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
@@ -38,20 +39,42 @@ class DashboardController extends AbstractDashboardController
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('<img src="/images/icon.svg" alt="" width="80px"/>')
+            ->setTitle('<img src="/images/icon.svg" width="50">')
             ->setFaviconPath('/images/icon.svg')
-            // ->renderSidebarMinimized()
-            ->renderContentMaximized();
+            ->renderContentMaximized()
+            ->setLocales([
+                'en' => '🇬🇧 English',
+                'pl' => '🇵🇱 Polski'
+            ]);
     }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::section('Assets');
-        yield MenuItem::linkToCrud('Rooms', 'fa fa-list', Room::class);
-        yield MenuItem::section('Users');
-        yield MenuItem::linkToCrud('Users', 'fa fa-users', User::class);
-        yield MenuItem::linkToRoute('Back to app', 'fa fa-link', 'app_room');
+        yield MenuItem::linkToCrud('Locations', 'fas fa-cubes', Room::class);
+        yield MenuItem::section('Utilisateur');
+        yield MenuItem::linkToCrud('Utilisateur', 'fas fa-users', User::class);
+        yield MenuItem::linkToRoute('Back to app', 'fas fa-arrow-left', 'app_room');
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        // Usually it's better to call the parent method because that gives you a
+        // user menu with some menu items already created ("sign out", "exit impersonation", etc.)
+        // if you prefer to create the user menu from scratch, use: return UserMenu::new()->...
+        return parent::configureUserMenu($user)
+            // use the given $user object to get the user name
+            ->setName($user->getFullname())
+            // use this method if you don't want to display the name of the user
+            ->displayUserName(false)
+
+            // you can return an URL with the avatar image
+            ->setAvatarUrl('/uploads/users/'. $user->getImage())
+
+            ->addMenuItems([
+                MenuItem::linkToLogout('Logout', 'fa fa-sign-out'),
+            ]);
     }
 }
